ac4a7c33dfde0a96d159b0db88c56aca7f4a8b89
jasondzheng <jasondzheng@gmail.com>
2016-06-20 19:43:31 -0500

    Reorganized static map entity assets to be grouped by containing map

--- src/loaders/mapLoader.js
+++ src/loaders/mapLoader.js
@@ -19,7 +19,7 @@ MapLoader.STATIC_MAP_ENTITY_DIR = '../assets/img/staticEntities/';
 MapLoader.load = function(mapName, opt_callback) {
 	JSONLoader.load(MapLoader.DIR + mapName + '.json', function(mapData) {
 		TileLoader.load(MapLoader.TILESET_DIR + mapData.tileset, function(tileset) {
-			MapLoader._helperLoadStaticMapEntities(mapData, function() {
+			MapLoader._helperLoadStaticMapEntities(mapName, mapData, function() {
 				if (opt_callback) {
 					opt_callback(new Map(mapData.name, mapData.data, mapData.width, 
 							tileset, mapData.dummyTile, mapData.staticMapInstances));
@@ -38,12 +38,12 @@ MapLoader.unload = function(map) {
 
 
 // Helper to load static map entities
-MapLoader._helperLoadStaticMapEntities = function(mapData, callback) {
+MapLoader._helperLoadStaticMapEntities = function(mapName, mapData, callback) {
 	var entityImageSpecs = {};
 	for (var entityKey in mapData.staticMapEntities) {
 		if (mapData.staticMapEntities.hasOwnProperty(entityKey)) {
 			entityImageSpecs[mapData.staticMapEntities[entityKey].sprite] = 
-					MapLoader.STATIC_MAP_ENTITY_DIR + 
+					MapLoader.STATIC_MAP_ENTITY_DIR + mapName + '/' +
 					mapData.staticMapEntities[entityKey].sprite + '.png';
 		}
 	}
