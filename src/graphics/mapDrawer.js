dce3f2dbbe45a63b14055965046a20f138eb6735
jasondzheng <jasondzheng@gmail.com>
2016-07-04 17:59:36 -0500

    Removed Trapeze to improve efficiency

--- src/graphics/mapDrawer.js
+++ src/graphics/mapDrawer.js
@@ -10,39 +10,25 @@ MapDrawer.BASE_HEIGHT = 59;
 
 // The number of rows to show.
 MapDrawer.TOTAL_ROWS = 10;
+MapDrawer.HALF_ROWS = MapDrawer.TOTAL_ROWS / 2;
 
 // The shrinkage coefficients.
 MapDrawer.SHRINKAGE = {
-	WIDTH: 1.5 / MapDrawer.TOTAL_ROWS, 
 	HEIGHT: 0.05
 };
 
 // The fixed width of all square tiles.
 MapDrawer.TILE_DIM = 96;
 
-// The number of tiles on the bottom of the screen. Deferred calc due to
-// dependency on ScreenProps.
-MapDrawer.NUM_TILES_BOTTOM;
+// The number of tiles per row
+MapDrawer.NUM_TILES_IN_ROW;
+MapDrawer.NUM_TILES_IN_ROW_HALF;
 
-// The number of tiles on the top of the screen. Deferred calc due to
-// dependency on ScreenProps.
-MapDrawer.NUM_TILES_TOP;
-
-// The top row tile width. Deferred calc due to dependency on ScreenProps.
-MapDrawer.TOP_ROW_TILE_WIDTH;
-
-// A canvas to draw onto for individual rows.
-MapDrawer.ROW_CANVAS;
 
 // Deferred initializer
 MapDrawer._deferredConstInit = function() {
-	MapDrawer.NUM_TILES_BOTTOM = ScreenProps.EXP_WIDTH / MapDrawer.TILE_DIM;
-	MapDrawer.NUM_TILES_TOP = MapDrawer.NUM_TILES_BOTTOM + 
-			MapDrawer.TOTAL_ROWS * MapDrawer.SHRINKAGE.WIDTH;
-	MapDrawer.TOP_ROW_TILE_WIDTH = ScreenProps.EXP_WIDTH / 
-			MapDrawer.NUM_TILES_TOP;
-	MapDrawer.ROW_CANVAS = document.createElement('canvas');
-	MapDrawer.ROW_CANVAS.height = MapDrawer.TILE_DIM;
+	MapDrawer.NUM_TILES_IN_ROW = ScreenProps.EXP_WIDTH / MapDrawer.TILE_DIM;
+	MapDrawer.NUM_TILES_IN_ROW_HALF = Math.ceil(MapDrawer.NUM_TILES_IN_ROW / 2);
 };
 
 
@@ -52,36 +38,21 @@ MapDrawer.drawMap = function(ctx, map, viewerX, viewerY) {
 	var viewerXWhole = Math.floor(viewerX), viewerXFrac = viewerX - viewerXWhole,
 			viewerYWhole = Math.floor(viewerY), viewerYFrac = viewerY - viewerYWhole;
 	// Calculate dependent constants if unset
-	if (MapDrawer.NUM_TILES_BOTTOM == undefined) {
+	if (MapDrawer.NUM_TILES_IN_ROW == undefined) {
 		MapDrawer._deferredConstInit();
 	}
 	var currYPos = MapDrawer._helperCalcScreenY(-1 - viewerYFrac);
 	for (var i = -1; i < MapDrawer.TOTAL_ROWS + 2; i++) {
 		var viewerI = i - viewerYFrac;
 		var nextYPos = MapDrawer._helperCalcScreenY(viewerI + 1);
-		var numTilesToDraw = MapDrawer.NUM_TILES_TOP - 
-				viewerI * MapDrawer.SHRINKAGE.WIDTH;
-		var tileTopWidth = MapDrawer.TOP_ROW_TILE_WIDTH * 
-				MapDrawer.NUM_TILES_TOP / numTilesToDraw;
-		var tileBotWidth = MapDrawer.TOP_ROW_TILE_WIDTH * MapDrawer.NUM_TILES_TOP / 
-				(MapDrawer.NUM_TILES_TOP - (viewerI + 1) * MapDrawer.SHRINKAGE.WIDTH);
-		var halfTilesToDraw = Math.ceil(numTilesToDraw / 2);
-		MapDrawer.ROW_CANVAS.width = (halfTilesToDraw + 1) * 2 * MapDrawer.TILE_DIM;
-		var rowCtx = MapDrawer.ROW_CANVAS.getContext('2d');
-		for (var j = -halfTilesToDraw - 1; j < halfTilesToDraw + 1; j++) {
+		for (var j = -MapDrawer.NUM_TILES_IN_ROW_HALF - 1; 
+				j < MapDrawer.NUM_TILES_IN_ROW_HALF + 1; j++) {
 			var viewerJ = j - viewerXFrac;
-			rowCtx.drawImage(MapDrawer._helperGetImgForTileCoords(map, 
-					j + viewerXWhole, i - MapDrawer.TOTAL_ROWS / 2 + viewerYWhole), 
-					MapDrawer.ROW_CANVAS.width / 2 + viewerJ * MapDrawer.TILE_DIM, 0);
+			ctx.drawImage(MapDrawer._helperGetImgForTileCoords(map, 
+					j + viewerXWhole, i - MapDrawer.HALF_ROWS + viewerYWhole), 
+					ScreenProps.EXP_WIDTH / 2 + viewerJ * MapDrawer.TILE_DIM, currYPos,
+					MapDrawer.TILE_DIM, nextYPos - currYPos);
 		}
-		/*
-		ImgUtils.drawTrapezium(ctx, MapDrawer.ROW_CANVAS, 
-				ScreenProps.EXP_WIDTH / 2 + tileTopWidth * (-halfTilesToDraw - 1), 
-				ScreenProps.EXP_WIDTH / 2 + tileBotWidth * (-halfTilesToDraw - 1), 
-				currYPos, tileTopWidth * (halfTilesToDraw + 1) * 2, 
-				tileBotWidth * (halfTilesToDraw + 1) * 2, nextYPos - currYPos);
-		*/
-		ctx.drawImage(MapDrawer.ROW_CANVAS,0,currYPos);
 		currYPos = nextYPos;
 	}
 };
@@ -104,32 +75,23 @@ MapDrawer.drawEntities = function(ctx, map, viewerX, viewerY) {
 		var footYGridPos = 
 				(MapDrawer.TOTAL_ROWS + entitiesToDraw[i].getCollisionHeight()) / 2;
 		var centerYRealGridPos = entitiesToDraw[i].y - 
-				viewerY + MapDrawer.TOTAL_ROWS / 2;
+				viewerY + MapDrawer.HALF_ROWS;
 		var footYRealGridPos = entitiesToDraw[i].y + 
 				entitiesToDraw[i].getCollisionHeight() / 2 - viewerY + 
-				MapDrawer.TOTAL_ROWS / 2;
+				MapDrawer.HALF_ROWS;
 		var realYDiff = MapDrawer._helperCalcScreenY(footYGridPos) - 
-				MapDrawer._helperCalcScreenY(MapDrawer.TOTAL_ROWS / 2);
+				MapDrawer._helperCalcScreenY(MapDrawer.HALF_ROWS);
 		var gridYDiff = MapDrawer._helperCalcScreenY(footYRealGridPos) - 
 				MapDrawer._helperCalcScreenY(centerYRealGridPos);
-		var yScale = 1;//gridYDiff / realYDiff;
-		var widthAtMidFoot = MapDrawer.TOP_ROW_TILE_WIDTH * 
-				MapDrawer.NUM_TILES_TOP / (MapDrawer.NUM_TILES_TOP - 
-				(footYGridPos) * MapDrawer.SHRINKAGE.WIDTH);
-		var widthAtRealFoot = MapDrawer._helperGetYBlockWidth(
-				entitiesToDraw[i].y + entitiesToDraw[i].getCollisionHeight() / 2, 
-				viewerY);
-		var xScale = widthAtRealFoot / widthAtMidFoot;
 		var centerLoc = MapDrawer._helperLocatePixel(entitiesToDraw[i].x, 
 				entitiesToDraw[i].y + entitiesToDraw[i].getCollisionHeight() / 2, 
-				viewerX, viewerY, MapDrawer._helperGetYBlockWidth(entitiesToDraw[i].y + 
-						entitiesToDraw[i].getCollisionHeight() / 2, viewerY));
+				viewerX, viewerY, MapDrawer.TILE_DIM);
 		// check if the boundingBox is in bounds
 		var boundingRect = {
-			x: centerLoc.x - entitiesToDraw[i].getCenter().x * xScale,
-			y: centerLoc.y - entitiesToDraw[i].getSprite().height * yScale,
-			width: entitiesToDraw[i].getSprite().width * xScale,
-			height: entitiesToDraw[i].getSprite().height * yScale
+			x: centerLoc.x - entitiesToDraw[i].getCenter().x,
+			y: centerLoc.y - entitiesToDraw[i].getSprite().height,
+			width: entitiesToDraw[i].getSprite().width,
+			height: entitiesToDraw[i].getSprite().height
 		};
 		if (MapDrawer._helperRectOnScreen(boundingRect)) {
 			ctx.drawImage(entitiesToDraw[i].getSprite(), boundingRect.x, 
@@ -152,15 +114,8 @@ MapDrawer._helperLocatePixel = function(x, y, viewerX, viewerY, yBlockWidth) {
 	var result = {};
 	result.x = ScreenProps.EXP_WIDTH / 2 + (x - viewerX) * yBlockWidth;
 	result.y = MapDrawer._helperCalcScreenY(
-			y - (viewerY - MapDrawer.TOTAL_ROWS / 2));
+			y - (viewerY - MapDrawer.HALF_ROWS));
 	return result;
-};
-
-// Helper function to get block width.
-MapDrawer._helperGetYBlockWidth = function(y, viewerY) {
-	return MapDrawer.TOP_ROW_TILE_WIDTH * MapDrawer.NUM_TILES_TOP / 
-			(MapDrawer.NUM_TILES_TOP - (y - (viewerY - MapDrawer.TOTAL_ROWS / 2)) * 
-			MapDrawer.SHRINKAGE.WIDTH)
 };
 
 
